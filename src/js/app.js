c7a435973226a4c49f007f89da5fcc736afed2df
MichaelQQ <mingsian.tu@vpon.com>
2016-05-03 14:40:39 +0800

    add webpack and rimraf

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+function add(a, b) {
+  return a + b;
+}
+
+export function sum(...args) {
+  // return args.reduce(add, 0);
+}
---
+++ webpack.dev.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  context: __dirname + "/src/js",
+  entry: "./app.js",
+
+  output: {
+    filename: "app.js",
+    path: __dirname + "/dist",
+  },
+};

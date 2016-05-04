8912fc085e0e67dfdc58897f2b15bef3b2aa81a8
MichaelQQ <mingsian.tu@vpon.com>
2016-05-04 15:32:02 +0800

    update development env

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build/js'),
     filename: 'app.js',
-    publicPath: '/static/'
+    publicPath: '/js/',
   },
   module: {
     loaders: [{

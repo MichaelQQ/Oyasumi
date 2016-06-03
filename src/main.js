1438077aa2ab57c22e53194f6ba27d3c1c945dbc
MichaelQQ <mr.tu@michaelqq.com>
2016-06-04 00:48:42 +0800

    fixed no-semicolon error

--- src/main.js
+++ src/main.js
@@ -34,12 +34,12 @@ const mainReducer = combineReducers({
  * Logs all actions and states after they are dispatched.
  */
 const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  const result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
+  console.group(action.type);
+  console.info('dispatching', action);
+  const result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd(action.type);
+  return result;
 };
 
 const sagaMiddleware = createSagaMiddleware();

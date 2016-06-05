145a6f476f1c73ec48833cf50e22020a5441cb7e
MichaelQQ <mr.tu@michaelqq.com>
2016-06-05 16:37:14 +0800

    add flickr router

--- server/flickr_router.js
+++ server/flickr_router.js
@@ -1,15 +1,26 @@
 import 'isomorphic-fetch';
+import co from 'co';
 import express from 'express';
 
 const router = express.Router();
+const FLICKR_ID = process.env.FLICKR_ID;
+const FLICKR_URI =
+  `https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&api_key=${FLICKR_ID}`;
 
-router.get('/flickr', (req, res) => {
-  res.end('hi api!');
-});
+function* fetchFlickr(query) {
+  const keys = Object.keys(query);
+  const requestUri = keys.reduce((prev, now) => `${prev}&${now}=${query[now]}`, FLICKR_URI);
+  const response = yield fetch(requestUri);
+  const data = yield response.json();
+  return data;
+}
+
 router.post('/flickr', (req, res) => {
-  console.log('method:', req.body.method);
-  console.log('value:', req.body.value);
-  res.end('success');
+  co(function* () {
+    const data = yield fetchFlickr(req.body);
+    res.send(data);
+    res.end();
+  });
 });
 
 export default router;

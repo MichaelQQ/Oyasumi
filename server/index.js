c2cf5084133553b9e557efe429b235c0a83f7502
MichaelQQ <mr.tu@michaelqq.com>
2016-06-22 21:27:36 +0800

    update server index.js to correctly deploy in heroku

--- server/index.js
+++ server/index.js
@@ -1,33 +1,25 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
-// import webpack from 'webpack';
-// import config from '../webpack.dev.config';
 import path from 'path';
 
 import instagramRouter from './instagram_router';
 import flickrRouter from './flickr_router';
 
-process.chdir('../');
 const port = process.env.PORT || 7788;
 const app = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
-
-// const compiler = webpack(config);
 app.use(express.static('public'));
 app.use(express.static('build'));
-// app.use(require('webpack-dev-middleware')(compiler, {
-//   noInfo: true,
-//   publicPath: config.output.publicPath,
-// }));
 
 app.use('/api', flickrRouter);
 app.use('/api', instagramRouter);
 
-app.get('/*', (req, res) => res.sendFile(path.join(process.cwd(), 'index.html')));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 app.listen(port, () => {
+  console.log(__dirname);
   console.log(`Listen at port: ${port}`);
 });
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: 'source-map',
+  // devtool: 'source-map',
   resolve: {
     root: __dirname + '/src'
   },
